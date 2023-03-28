d902c939f13bb075b5c36fbfda4b9cadfd3eec93
CodeSandbox <support@codesandbox.io>
2023-03-28 02:13:00 +0000

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,28 @@
+const student = {
+  name: "Lucas Souza",
+  age: 24,
+  couse: "Linguagem de Programação",
+  inSubscribed: true
+};
+
+const teacher = {
+  name: "Juvenal Timotin",
+  enrollment: 1141141,
+  matter: "Banco de Dados"
+};
+
+console.log(`
+O estudante ${student.name} de ${student.age} anos
+esta inscrito(${student.inSubscribed}) no curso de ${student.
+
+couse} do professor ${teacher.name}
+`);
+
+//array vazio
+let peoples = [];
+
+//inserindo um object no array
+peoples = [student, teacher];
+
+console.log(peoples[1]);
+console.log(peoples[0])
